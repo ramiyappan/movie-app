64db86aa98b33e4b72e72c5050a2ea6362bfd165
ramiyappan <ramiyappan98@gmail.com>
2024-04-02 17:07:26 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchIcon from './search.svg'
+import { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
+
+const API_URL = 'http://www.omdbapi.com?apikey=ad8d4f8'
 
 function App() {
+  const [movies, setMovies] = useState([]) // movies is the state value and setMovies is the function that sets the state of movies
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const searchMovies = async(title) => {
+    const response = await fetch(`${API_URL}&s=${title}`) // fetch data from the API
+    const data = await response.json()
+
+    // set the movies array to store all the movies data from the API
+    setMovies(data.Search)
+  }
+
+  // useEffect() loads once everytime the page is rendered
+  useEffect(() => {
+    searchMovies('Batman')
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      {/* Title */}
+      <h1>MovieLand</h1>
+
+      {/* Search Bar */}
+      <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyDown={(e) => {
+            if(e.key==='Enter') 
+              searchMovies(searchTerm)
+          }}
+        />
+        {/* search icon */}
+        <img 
+          src={SearchIcon}
+          alt='search'
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {/* Generate the MovieCard component for each element, if movies array is not empty  */}
+      {movies?.length > 0
+        ? (
+          <div className='container'>
+            {movies.map((movie) => (
+              <MovieCard movie={movie}/>
+            ))}
+          </div>
+        ) : (
+          <div className='empty'>
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
+
     </div>
   );
 }
